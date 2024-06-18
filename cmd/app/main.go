4df0c20a83cb14fe62a3132221fa21918cb50abf
zyh <zyh@verysimon.cn>
2024-06-18 13:42:21 +0800

    [new]

---
+++ cmd/app/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	
+	"fmt"                // 引入基础格式化输出包
+	"os"                 // 引入操作系统交互功能包
+)
+
+func main() {
+	
+}
--- html/static/script/utility/tables.js
+++ html/static/script/utility/tables.js
@@ -64,15 +64,58 @@ function modifyTable(tableId, tableData) {
 }
 
 // 级联表格检查表头和数据是否匹配
+// 表头和数据顺序不一样的情况仍然返回true
+//TODO: 后续创建表格时按照顺序来
 function checkCascadeHeaderData(header, data) {
-    return data.every(item => 
-        Object.keys(item).filter(key => key !== 'parentId' && key !== 'children').length === header.length
-    );
+    function extractNames(itemArray) {
+        let names = [];
+        itemArray.forEach(item => {
+            if (item.name !== 'parentId' && item.name !== 'children') {
+                names.push(item.name);
+            }
+            if (item.name === 'children' && Array.isArray(item.value)) {
+                item.value.forEach(childArray => {
+                    names.push(extractNames(childArray));
+                });
+            }
+        });
+        return names;
+    }
+
+    function extractHeader(header) {
+        return header.map(h => h.name)
+    }
+
+    function checkNames(itemNames, header) {
+        //如果itemNames中都不是Array元素则是是叶子节点，那么检查是否和表头匹配
+        if (!header.every(h => itemNames.includes(h))) {
+            return false;
+        };
+        itemNames.forEach(name => {
+            if (Array.isArray(name)) {
+                if (!checkNames(name, header))
+                    return false;
+            } else {
+                if (!header.includes(name))
+                    return false
+            }
+        });
+        return true;
+    }
+    return data.every(itemArray => {
+        const itemNames = extractNames(itemArray);
+        const itemHeader = extractHeader(header)
+        return checkNames(itemNames, itemHeader);
+    });
 }
 
 //创建级联表格
 //返回一个jq的table对象
 //tableId: 表格id, tableHeader: 表头, tableData: 表格数据
+//header的格式:
+//[{"name":"header1", "propertyIndex":1, "diaplayDefault":true},{...},{...}]
+//data的格式:
+//[[{"name":"id","value":1},{"name":"parentId","value":0},{...},{...}],[...],[...]]
 function createCascadeTable(tableId, tableHeader, tableData) {
     if (!checkCascadeHeaderData(tableHeader, tableData)) {
         console.error(`表头和数据不匹配Id: ${tableId}, Header: ${tableHeader}, Data: ${tableData}`);
@@ -84,32 +127,46 @@ function createCascadeTable(tableId, tableHeader, tableData) {
 
     var $tr = $('<tr></tr>');
     tableHeader.forEach(header => {
-        var $th = $(`<th>${header}</th>`);
+        var $th = $(`<th>${header.name}</th>`);
         $tr.append($th);
     });
     $thead.append($tr);
     $table.append($thead);
 
     function addRow(data, level) {
-        data.forEach(item => {
-            var $tr = $(`<tr data-id="${item.id}" data-parent-id="${item.parentId}" class="cascade-table-level-${level}"></tr>`);
-            var $arrow = (item.children.length) > 0 ? $('<span class="arrow">▼</span>'): '';
-            // 遍历item除了.parentId和.children的属性
-            Object.keys(item).filter(key => key !== 'parentId' && key !== 'children').forEach((key, index) => {
-                var $td = $(`<td></td>`);
+        data.forEach(itemArray => {
+            const item = Object.fromEntries(itemArray.map(i => [i.name, i.value]));
+            var $tr = $(`<tr data-id="${item["id"]}" data-parent-id="${item["parentId"]}" id="cascade-table-level-${level}"></tr>`);
+            var $arrow = item['children'] && item['children'].length > 0 ? $('<span class="arrow">▼</span>') : '';
+            
+            // 遍历thead中的th的text作为key，来寻找item中的元素
+            const $headers = $thead.find('th')
+            $headers.each(function(index) {
+                const key = $(this).text();
+                var $td = $(`<td></td>`).css('padding-left', `${20 * level}px`);
                 //在第一个td中添加箭头
                 if (index === 0) {
                     $td.append($arrow).append(item[key]);
                 } else {
                     $td.append(item[key]);
                 }
-                
                 $tr.append($td);
             });
-    
+
+            // //遍历item除了key是parentId和children的value
+            // Object.keys(item).filter(key => key !== 'parentId' && key !== 'children').forEach((key, index) => {
+            //     var $td = $(`<td></td>`);
+            //     //在第一个td中添加箭头
+            //     if (index === 0) {
+            //         $td.append($arrow).append(item[key]);
+            //     } else {
+            //         $td.append(item[key]);
+            //     }
+            //     $tr.append($td);
+            // });
             $tbody.append($tr);
-            if (item.children.length > 0) {
-                addRow(item.children, level + 1);
+            if (item['children'] && item['children'].length > 0) {
+                addRow(item['children'], level + 1);
             }
         });
     }
@@ -121,12 +178,12 @@ function createCascadeTable(tableId, tableHeader, tableData) {
         var $arrow = $(this);
         var $tr = $arrow.closest('tr');
         var id = $tr.data('id');
-        var level = parseInt($tr.attr('class').match(/level-(\d+)/)[1], 10);
+        var level = parseInt($tr.attr('id').match(/level-(\d+)/)[1], 10);
 
         if ($arrow.text() === '▶') {
             $arrow.text('▼');
             showChildren(id, level);
-        } else if($arrow.text() === '▼'){
+        } else if ($arrow.text() === '▼') {
             $arrow.text('▶');
             hideChildren(id);
         }
@@ -138,7 +195,9 @@ function createCascadeTable(tableId, tableHeader, tableData) {
         $table.find(`tr[data-parent-id=${parentId}]`).each(function () {
             const $child = $(this);
             $child.show();
-
+            
+            // 如果箭头是▼，则展示子节点
+            // TODO: 不用▼来判断，而是用一个变量来判断
             if ($child.find('.arrow').text() === '▼') {
                 showChildren($child.data('id'), level + 1);
             }
@@ -153,4 +212,25 @@ function createCascadeTable(tableId, tableHeader, tableData) {
         });
     }
 
+}
+
+// 给级联表格添加一列，都是按钮（可能不只一个按钮），按钮文本为buttonText，点击按钮时调用callback
+// tableId: 表格id, buttonText: 按钮文本
+// callback: 点击按钮时调用的函数数组，函数参数为当前行的id
+function addTableButton($table, title, buttonTexts, callback) {
+    var $thead = $table.children('thead');
+    var $tbody = $table.children('tbody');
+    $thead.find('tr').append(`<th>${title}</th>`);
+    $tbody.children('tr').each(function () {
+        var $tr = $(this);
+        var $td = $(`<td></td>`);
+        buttonTexts.forEach((buttonText, index) => {
+            let $button = $(`<a>${buttonText}</a>`);
+            $button.on('click', function () {
+                callback[index]($tr.data('id'));
+            });
+            $td.append($button);
+        });
+        $tr.append($td);
+    });
 }
